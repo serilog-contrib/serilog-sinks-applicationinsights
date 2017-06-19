85bd9a38a1fbd5e737c95cc71cda7ecf94c7f39f
Christos Matskas <christos.matskas@gmail.com>
2017-06-20 00:51:10 +0100

    Migration to csproj. Added sample app to test the code

---
+++ Sample/AppInsightsDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using Serilog;
+using System;
+using System.Threading;
+
+namespace AppInsightsDemo
+{
+    class Program
+    {
+        const string INSTRUMENTATION_KEY = "<Your AI instrumentation key";
+        static void Main(string[] args)
+        {
+
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo
+            .ApplicationInsightsTraces(INSTRUMENTATION_KEY)
+            .CreateLogger();
+
+            try
+            {
+                Log.Debug("Getting started");
+                Log.Information("Hello {Name} from thread {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), Thread.CurrentThread.ManagedThreadId);
+                Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
+
+                Fail();
+            }
+            catch (Exception e) 
+            {
+                Log.Error(e, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
+            Console.ReadKey();
+        }
+
+        static void Fail()
+        {
+            throw new DivideByZeroException();
+        }
+
+    }
+}
