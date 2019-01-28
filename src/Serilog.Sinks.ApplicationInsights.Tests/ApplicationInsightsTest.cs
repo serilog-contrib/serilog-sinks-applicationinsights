1eb4fb4f6e456f303ccb0ecae29743dc71e271da
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2019-01-28 10:43:53 +0000

    Trace does not include LogLevel and MessageTemplate by default (#36)

---
+++ src/Serilog.Sinks.ApplicationInsights.Tests/ApplicationInsightsTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Serilog.Sinks.ApplicationInsights.Tests
+{
+    public abstract class ApplicationInsightsTest
+    {
+        private readonly UnitTestTelemetryChannel _channel;
+
+        public ApplicationInsightsTest()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ApplicationInsightsTraces("")
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            var tc = TelemetryConfiguration.Active;
+
+            tc.TelemetryChannel = _channel = new UnitTestTelemetryChannel();
+        }
+
+        protected List<ITelemetry> SubmittedTelemetry => _channel.SubmittedTelemetry;
+
+        protected ITelemetry LastSubmittedTelemetry => _channel.SubmittedTelemetry.LastOrDefault();
+
+        protected TraceTelemetry LastSubmittedTraceTelemetry => 
+            _channel.SubmittedTelemetry
+                .Where(t => t is TraceTelemetry)
+                .Select(t => (TraceTelemetry)t)
+                .LastOrDefault();
+    }
+}
---
+++ src/Serilog.Sinks.ApplicationInsights.Tests/FormattingTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.ApplicationInsights.Channel;
+using Xunit;
+
+namespace Serilog.Sinks.ApplicationInsights.Tests
+{
+    public class FormattingTests : ApplicationInsightsTest
+    {
+        [Fact]
+        public void Log_level_is_not_in_trace_custom_property()
+        {
+            Log.Information("test");
+
+            Assert.False(LastSubmittedTraceTelemetry.Properties.ContainsKey("LogLevel"));
+        }
+
+        [Fact]
+        public void Message_template_is_not_in_trace_custom_property()
+        {
+            Log.Information("test");
+
+            Assert.False(LastSubmittedTraceTelemetry.Properties.ContainsKey("MessageTemplate"));
+        }
+    }
+}
