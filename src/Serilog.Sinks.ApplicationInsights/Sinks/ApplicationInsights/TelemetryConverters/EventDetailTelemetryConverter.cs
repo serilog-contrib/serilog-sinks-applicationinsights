cf7f2889957826d6ad5562ae99d7db61617e3125
MoeHamdan <moh.hamdan@outlook.com>
2019-08-19 13:23:43 +0300

    adding event detail telemetry converter to include log level and template

---
+++ src/Serilog.Sinks.ApplicationInsights/Sinks/ApplicationInsights/TelemetryConverters/EventDetailTelemetryConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.ApplicationInsights.DataContracts;
+using Serilog.Events;
+
+namespace Serilog.Sinks.ApplicationInsights.Sinks.ApplicationInsights.TelemetryConverters
+{
+    public class EventDetailTelemetryConverter : EventTelemetryConverter
+    {
+        public override void ForwardPropertiesToTelemetryProperties(LogEvent logEvent, ISupportProperties telemetryProperties, IFormatProvider formatProvider)
+        {
+            ForwardPropertiesToTelemetryProperties(logEvent, telemetryProperties, formatProvider,
+                includeLogLevel: true,
+                includeRenderedMessage: true,
+                includeMessageTemplate: true);
+        }
+    }
+}
